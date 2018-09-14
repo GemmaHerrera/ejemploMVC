a51ab1bd21e0fe8414e8af53ba8406d38c342be8
GemmaHerrera <42845588+GemmaHerrera@users.noreply.github.com>
2018-09-15 07:31:20 +1400

    inicio del proyecto

---
+++ Tienda/App_Code/BLL/Prendas.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DAL;
+
+/// <summary>
+/// Descripción breve de Prendas
+/// </summary>
+/// 
+namespace BLL
+{
+    public class Prendas
+{
+    public Prendas()
+    {
+        //
+        // TODO: Agregar aquí la lógica del constructor
+        //
+    }
+        public static long InsertPrenda(PrendasInfo info)
+        {
+
+            Prenda Prenda = new Prenda(); //Objeto creado de la clase DAL//Se pasa la info a cada propiedad del objeto.
+
+            Prenda.FechaCreacion = DateTime.Now;
+
+            Prenda.IDPrenda = info.IDPrenda;
+            Prenda.Color = info.Color;
+            Prenda.Tipo = info.Tipo;
+            Prenda.Precio = info.Precio;
+            Prenda.Talla = info.Talla;
+            Prenda.IDCateg = info.IDCateg;
+            Prenda.IDTemp = info.IDTemp;
+            //objeto de data context
+            DataClassesDataContext dc = new DataClassesDataContext();
+
+            dc.Prenda.InsertOnSubmit(Prenda);
+            dc.SubmitChanges(); //ejecuta cambios
+
+            return Prenda.IDPrenda;
+
+        }
+}
+    public class PrendasInfo
+    {
+        public long IDPrenda { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        
+        public string Color { get; set; }
+        public string Tipo { get; set; }
+        public decimal Precio { get; set; }
+        public string Talla { get; set; }
+        public long IDCateg { get; set; }
+        public long IDTemp { get; set; }
+    }
+
+  
+}
---
+++ Tienda/Controles/PrendasControl.ascx
@@ -0,0 +1,14 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="PrendasControl.ascx.cs" Inherits="Controles_PrendasControl" %>
+<asp:Label ID="Label1" runat="server" Text="Color "></asp:Label>
+<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+<p>
+    <asp:Label ID="Label2" runat="server" Text="Tipo"></asp:Label>
+    <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+</p>
+<asp:Label ID="Label3" runat="server" Text="Precio"></asp:Label>
+<asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+<p>
+    <asp:Label ID="Label4" runat="server" Text="Talla"></asp:Label>
+    <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+</p>
+
